8b5af1ec8807f29886808aa6e1f5e0cbff896117
FiBoot <fiboot89@gmail.com>
2020-06-28 13:10:41 +0200

    :sparkles: noice

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-module.exports.HeroBook = require('./hero-book/main');
-module.exports.TextRPG = require('./text-rpg/main');
 module.exports.basic = require('./basics/main');
 module.exports.bigText = require('./big-text/main');
-module.exports.random = require('./random/main');
-module.exports.nameGenerator = require('./name-generator/main');
 module.exports.fetchImage = require('./fetch-image/main');
+module.exports.HeroBook = require('./hero-book/main');
+module.exports.nameGenerator = require('./name-generator/main');
+module.exports.noice = require('./noice/main');
+module.exports.random = require('./random/main');
+module.exports.TextRPG = require('./text-rpg/main');
